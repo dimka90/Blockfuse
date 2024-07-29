028d79841b7641b89e87f0b9a3cb6b15cc0b0258
dimka90 <dimkayilrit@gmail.com>
2024-07-29 11:02:44 +0100

    fixing key error bug

--- two_sum.py
+++ two_sum.py
@@ -15,7 +15,12 @@ def twoSum(array:List[int], target: int  ) -> List[int]:
     for index, number in enumerate(array):
         
         difference = target - number
-
         # check for the particular number in the hashed map to store
+        if difference in num_index:
+            return [num_index[difference, index]]
 
-       
+        num_index[number] = index
+    return []
+
+
+twoSum([2,7,11,15],9)
